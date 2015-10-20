26737cb633938973c13f54b2b4971eef3e23e41b
Leire Viseras <leireviseras@gmail.com>
2015-10-20 18:20:45 +0200

    Token & pin kudeatzeko gehikuntzen commit and push -a

---
+++ twitterBackup/src/grafikoa/Start.java
@@ -0,0 +1,14 @@
+package grafikoa;
+
+import logikoa.PinKudeaketa;
+
+public class Start {
+	public static void main(String[] args) {
+		if(PinKudeaketa.getPin().badagoFitx()){
+			OrrNagusia.bistaratu();
+		}else{
+			Has.bistaratu();
+		}
+		
+	}
+}
