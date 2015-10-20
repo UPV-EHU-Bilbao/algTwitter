9b70c374228bd939e7b7a0855b51a19eff9c0081
Leire Viseras <leireviseras@gmail.com>
2015-10-20 18:20:01 +0200

    Token & pin kudeatzeko gehikuntzak

--- twitterBackup/src/logikoa/PinKudeaketa.java
+++ twitterBackup/src/logikoa/PinKudeaketa.java
@@ -1,5 +1,24 @@
 package logikoa;
 
+import java.io.File;
+
 public class PinKudeaketa {
+	private static PinKudeaketa mPin = null;
+	private File tokenFitx = null;
+	
+	private PinKudeaketa(){}
+	public static synchronized PinKudeaketa getPin(){
+		if(mPin == null){
+			mPin = new PinKudeaketa();
+		}return mPin;
+	}
+	
+	public void eratuFitxategia(String pErab){
+		tokenFitx = new File("src/"+pErab+"token");
+	}
+	
+	public boolean badagoFitx(){
+		return tokenFitx.exists();
+	}
 	
 }
