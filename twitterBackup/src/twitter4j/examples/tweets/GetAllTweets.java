111f7f286890a9c20076e1c936210a3091e97967
galder542 <Galdervillaterron@25.149.214.46>
2015-11-11 10:46:34 +0100

    ALDAKETAK

--- twitterBackup/src/twitter4j/examples/tweets/GetAllTweets.java
+++ twitterBackup/src/twitter4j/examples/tweets/GetAllTweets.java
@@ -6,18 +6,27 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class GetAllTweets {
-		
+	
 		public static void main (String args[]) {
+			ConfigurationBuilder cb = new ConfigurationBuilder();
+			cb.setOAuthConsumerKey("");
+			cb.setOAuthConsumerSecret("");
+			cb.setOAuthAccessToken("");
+			cb.setOAuthAccessTokenSecret("");
+			//meter datos de twitter
 			Twitter twitter = new TwitterFactory().getInstance();
 		        ArrayList <Status> statuses = new ArrayList<Status>();
 		        int pageno = 1;
+		        long id = statuses.lastIndexOf(statuses);
 		        while (true) {
 		            try {
 		                int size = statuses.size();
-		                Paging page = new Paging(pageno++, 100);
+		                Paging page = new Paging(pageno++, 100).sinceId(id);
 		                statuses.addAll(twitter.getUserTimeline(page));
+		                id = statuses.lastIndexOf(statuses);
 		                if (statuses.size() == size || pageno==3) {
 		                    break;
 		                }
